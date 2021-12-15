bcddb02179e784304c723a8d4a5a573b774c074b
johnson88022 <johnson88022@gmail.com>
2021-12-15 14:22:46 +0800

    second commit

---
+++ try_except.py
@@ -0,0 +1,13 @@
+try:
+	num = input("請輸入一個數字")
+    num = int(num)
+    total = num+1
+    print(total)
+except ValueError:
+	print("輸入錯誤")
+except:
+	print("發生錯誤")
+else:
+	print("成功執行")
+finally:
+	print("程式結束")
